ca8cf6224584e46cfbb77b02e904f9fd9851240d
AlexEmrik <60000377+AlexEmrik@users.noreply.github.com>
2021-02-01 12:17:52 +0100

    Updates

---
+++ precache-manifest.0eb3965d3668b13c9dee3bb1b44cd360.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af13b5b4f5faec639c742f65a60d0b17",
+    "url": "/MittGymnasieArbete/index.html"
+  },
+  {
+    "revision": "8a53005438c5e393c6b6",
+    "url": "/MittGymnasieArbete/static/css/main.90153fae.chunk.css"
+  },
+  {
+    "revision": "4153dcb9e30748db5b9a",
+    "url": "/MittGymnasieArbete/static/js/2.e6a65e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MittGymnasieArbete/static/js/2.e6a65e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a53005438c5e393c6b6",
+    "url": "/MittGymnasieArbete/static/js/main.7ca11fd4.chunk.js"
+  },
+  {
+    "revision": "069569581c15a2b21ede",
+    "url": "/MittGymnasieArbete/static/js/runtime-main.288cb55b.js"
+  },
+  {
+    "revision": "8ca8c8e073b33b4db3ba09574880db08",
+    "url": "/MittGymnasieArbete/static/media/Moon.8ca8c8e0.svg"
+  },
+  {
+    "revision": "b2d96c24459d871ca5a269f259833bd0",
+    "url": "/MittGymnasieArbete/static/media/Silhouette.b2d96c24.svg"
+  },
+  {
+    "revision": "555355406fd5ad4544a501aa2bec197d",
+    "url": "/MittGymnasieArbete/static/media/Stars.55535540.svg"
+  },
+  {
+    "revision": "4fcefad7d16d37ba4cc86506d9c73e89",
+    "url": "/MittGymnasieArbete/static/media/Trees.4fcefad7.svg"
+  },
+  {
+    "revision": "b0ec457f517f29a35a8cf88006b502cc",
+    "url": "/MittGymnasieArbete/static/media/envelope.b0ec457f.svg"
+  },
+  {
+    "revision": "fe5d5c7a4703f85cc08d99003c0f3993",
+    "url": "/MittGymnasieArbete/static/media/fashion.fe5d5c7a.jpg"
+  }
+]);
